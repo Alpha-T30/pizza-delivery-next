b4063a1e6df1f5a8b6e6a54ef3100814388c9dc9
Alpha-T30 <mrhaquet20@gmail.com>
2022-03-06 21:41:43 +0600

    initial setup done,sass,tailwind,bootstrap5,css module added working fine

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import Style from "../styles/Navbar.module.scss";
+
+const Navbar = () => {
+  return (
+    <div className={`bg-orange-500 top-0  h-16 drop-shadow-sm sticky overflow-hidden shadow-md ${Style.cointainer}`}>
+      
+      
+       
+
+      
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Link from "next/link";
+import Script from "next/script";
+import Layout from "../components/Layout";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
